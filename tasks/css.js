73a920ca56267630f89c9c41b7f4435b4f9a27b1
sokolov alex <alex.sklv85@gmail.com>
2016-08-06 01:01:10 +0300

    tour cards

--- tasks/css.js
+++ tasks/css.js
@@ -10,9 +10,10 @@ module.exports = function() {
     var isProd = process.env.NODE_ENV === "prod";
 
     gulp.task('css', function() {
-        return gulp.src('src/css/*.css')
+        return gulp.src(['src/css/theme.css', 'src/css/*.less'])
             .pipe($.sourcemaps.init())
             .pipe($.concat('styles.css'))
+            .pipe($.less())
             .pipe($.sourcemaps.write())
             .pipe(gulp.dest('build/css'))
             .pipe(browserSync.reload({
