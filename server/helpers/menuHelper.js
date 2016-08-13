8df63620808eefadcb7ec87e3c1c399b65916101
sokolov alex <alex.sklv85@gmail.com>
2016-08-14 02:20:02 +0300

    add pages: games, avia

---
+++ server/helpers/menuHelper.js
@@ -0,0 +1,61 @@
+const welcome = () => {
+    return [{
+        title: 'Туры',
+        link: '#hot',
+        scroll: true
+    }, {
+        title: 'О нас',
+        link: '#about',
+        scroll: true
+    }, {
+        title: 'Команда',
+        link: '#team',
+        scroll: true
+    }, {
+        title: 'Заявка',
+        link: '#contact',
+        scroll: true
+    }, {
+        title: 'Прочее',
+        submenu: [{
+            title: 'Поиск авиабилетов',
+            link: '/aviakassa'
+        }, {
+            separator: true
+        }, {
+            title: 'Туристические игры',
+            link: '/games'
+        }]
+    }, {
+        title: 'Войти',
+        modal: true,
+        link: '#loginModal'
+    }];
+};
+
+const back = () => {
+    return [{
+        title: 'Назад',
+        link: '/'
+    }, {
+        title: 'Прочее',
+        submenu: [{
+            title: 'Поиск авиабилетов',
+            link: '/aviakassa'
+        }, {
+            separator: true
+        }, {
+            title: 'Туристические игры',
+            link: '/games'
+        }]
+    }, {
+        title: 'Войти',
+        modal: true,
+        link: '#loginModal'
+    }];
+};
+
+module.exports = {
+    welcome: welcome,
+    back: back
+};
--- server/routes/pages.js
+++ server/routes/pages.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var menuHelper = require('../helpers/menuHelper');
 
-/* GET users listing. */
 router.get('/', function (req, res) {
-    res.render('welcome');
+    res.render('welcome', { menu: menuHelper.welcome()});
+});
+
+router.get('/games', function (req, res) {
+    res.render('games', { menu: menuHelper.back()});
+});
+
+router.get('/aviakassa', function (req, res) {
+    res.render('aviakassa', { menu: menuHelper.back()});
 });
 
 module.exports = router;
