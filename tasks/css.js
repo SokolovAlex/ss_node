1d982394ebb25b008ae59f4dc61d3f9d05ef1621
sokolov alex <alex.sklv85@gmail.com>
2019-04-26 01:53:40 +0300

    change css order in lib

--- tasks/css.js
+++ tasks/css.js
@@ -7,9 +7,9 @@ var $ = require('gulp-load-plugins')();
 module.exports = function() {
   gulp.task('vendor:styles', function() {
     return gulp.src([
+        'client/libs/css/font-awesome.css',
         'client/libs/css/bootstrap.css',
         'client/libs/css/bootstrap-datepicker3.css',
-        'client/libs/css/font-awesome.css'
       ])
       .on('data', (f) => console.log(' --> ', f.relative))
       .pipe($.concat('libs.css'))
